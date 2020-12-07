01c2f7ce6aa742c386f3471a344699e22bc68071
Robert <55789494+rabjaert@users.noreply.github.com>
2020-12-07 11:39:39 +0100

    ordnet mer på css, og la til wojak som favicon browser tab

--- client/src/components/KontorerForm.jsx
+++ client/src/components/KontorerForm.jsx
@@ -5,6 +5,9 @@ import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import { create } from '../utils/officeService';
 
+//lånt en del css fra https://www.w3schools.com/css/css_form.asp
+//syns det var vrient å style forms
+
 const FormContainer = styled.form`
     width: 500px;
     margin: 0 auto;
@@ -22,24 +25,41 @@ const Title = styled.h1`
 
 `
 const Input = styled.input`
-    width: 100%
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
     box-sizing: border-box;
-    background-color: blue;
+    
     
 `
 
 const FormLabel = styled.label`
     margin-top:10px;
     width: 100%
+    color: blue;
     
 `
 const Button = styled.button`
-margin-top: 10px;
-width: 120px;
-height: 100% ; 
-background-color: #4198e5; 
-color: white;
+    width: 100%;
+    background-color: #4198e5;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+
+  &:hover ${Button} {
+      background-color: #1fe7ed;
+  }
+
+
 `
+
+
 const CloseButton = styled.button`
 
 `
@@ -62,14 +82,16 @@ const KontorerForm = () => {
     if (!data.success) {
       setCloseBtnState(true);
       setError(data.message);
+      
     } else {
       setSuccess(true);
       setError(null);
+     
       console.log(formData);
       setTimeout(() => {
           
         history.push(`/kontorer`);
-      }, 2000);
+      }, 3000);
     }
   };
 
@@ -77,22 +99,22 @@ const KontorerForm = () => {
         <>
         <Styles.TitleBox><h1><Title>Velkommen til FG Rørleggerservice AS</Title></h1></Styles.TitleBox>
             <FormContainer onSubmit={handleSubmit(onSubmit)}>
-                <FormLabel htmlFor="name">Navn</FormLabel>
+                <FormLabel htmlFor="name">Name</FormLabel>
                 <Input
                     type="text"
                     name="name"
                     id="name"
-                    placeholder="name"
+                    placeholder="Please type in your office name"
                     ref={register({
                         required: true,
                     })}/>
 
-                <FormLabel htmlFor="number">Nummer</FormLabel>
+                <FormLabel htmlFor="number">Number</FormLabel>
                 <Input
                     type="number"
                     name="number"
                     id="number"
-                    placeholder="number"
+                    placeholder="Please type in your office number"
                     ref={register({
                         required: true,
                     })}/>
@@ -102,7 +124,7 @@ const KontorerForm = () => {
                     type="email"
                     name="email"
                     id="email"
-                    placeholder="email"
+                    placeholder="Please type in your office email"
                     ref={register({
                         required: true,
                     })}/>
@@ -112,7 +134,7 @@ const KontorerForm = () => {
                     type="text"
                     name="city"
                     id="city"
-                    placeholder="city"
+                    placeholder="Please type in the location of your office"
                     ref={register({
                         required: true,
                     })}/>
